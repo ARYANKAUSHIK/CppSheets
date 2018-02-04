0ddd005e870528812c5be993755440ab50a6c5b1
patrickf2000 <patrick_dev2000@outlook.com>
2018-02-04 12:54:33 -0500

    created separate stylesheet file

--- src/main.cxx
+++ src/main.cxx
@@ -1,11 +1,24 @@
 #include <QApplication>
+#include <QFile>
+#include <QTextStream>
 
 #include "window.hh"
 
 int main(int argc, char **argv) {
     QApplication app(argc,argv);
 
+    QFile css(":/style.css");
+    QString style = "";
+    if (css.open(QFile::ReadOnly)) {
+        QTextStream reader(&css);
+        while (!reader.atEnd()) {
+            style+=reader.readLine();
+        }
+        css.close();
+    }
+
     Window win;
+    win.setStyleSheet(style);
     win.show();
 
     return app.exec();
--- src/tablewidget.cxx
+++ src/tablewidget.cxx
@@ -3,8 +3,6 @@
 TableWidget::TableWidget() {
     this->setColumnCount(1000);
     this->setRowCount(1000);
-    this->setStyleSheet("QTableWidget::item:selected { background-color:white; color:black; "
-                        "border-color: blue; border-style:solid; border-width:2px; }");
 
     connect(this,&TableWidget::cellChanged,this,&TableWidget::onCellChanged);
 }
