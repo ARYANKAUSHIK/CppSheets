6c7e94136f67876961c52f1d73b5287c9826f557
patrickf2000 <patrick_dev2000@outlook.com>
2018-04-04 17:55:10 -0400

    created separate file for sheet item structure

--- src/parser/sheet_item.hh
+++ src/parser/sheet_item.hh
@@ -26,27 +26,22 @@
 // EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #pragma once
 
-#include <QString>
-#include <QStringList>
-#include <QVector>
-#include <QColor>
-#include <QFont>
+struct SheetItem {
+    int x;
+    int y;
+    QString data;
+    QColor bgColor;
+    QColor fgColor;
+    QString tooltip;
+    int colWidth;
+    int rowWidth;
+    int spanx;
+    int spany;
+    QFont font;
+};
 
-#include "tinyxml2.h"
-#include "sheet_item.hh"
-
-using namespace tinyxml2;
-
-class Parser {
-public:
-    static void createFile(QString filePath);
-	static QStringList pages(QString file);
-    static QVector<SheetItem> allItems(QString file, QString page);
-    static QVector<MathItem> allMathItems(QString file, QString page);
-    static void createPage(QString file, QString page);
-    static bool pageExists(QString file, QString pageName);
-    static void removePage(QString file, QString page);
-    static void setData(QString file, QString page, QVector<SheetItem> items);
-    static void setMathData(QString file, QString page, QVector<MathItem> items);
-    static XMLElement *getPageElement(XMLElement *root, QString title);
+struct MathItem {
+    int x;
+    int y;
+    QString equation;
 };
