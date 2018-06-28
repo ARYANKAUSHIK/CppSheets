c888b59915995070e5691e9e460b5d5b5471e814
patrickf2000 <patrick_dev2000@outlook.com>
2018-06-28 06:11:49 -0400

    re-organized repository

--- deploy_linux.sh
+++ deploy_linux.sh
@@ -3,10 +3,10 @@ cd build
 mkdir linux_bin
 cp src/CppSheets linux_bin
 cp xlnt/source/libxlnt.{so,so.1.2} linux_bin
-cp ../linux_install.sh linux_bin
-cp ../linux_uninstall.sh linux_bin
-cp ../linux_deploy_readme linux_bin/README
-cp ../CppSheets.desktop linux_bin
+cp ../linux/linux_install.sh linux_bin
+cp ../linux/linux_uninstall.sh linux_bin
+cp ../share/linux_deploy_readme linux_bin/README
+cp ../share/CppSheets.desktop linux_bin
 cp ../src/icons/kspread.png linux_bin/CppSheets.png
 cp ../COPYING* linux_bin
 
