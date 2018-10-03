c289680fe1380ea3625e6a23a7b6478296b971f4
patrickf2000 <patrick_dev2000@outlook.com>
2018-10-02 21:01:12 -0400

    relicensed program from BSD-3 to GPL-3

--- src/graphwin.cxx
+++ src/graphwin.cxx
@@ -1,3 +1,19 @@
+// This file is part of CppSheets.
+//
+// Copyright 2018 Patrick Flynn <patrick_dev2000@outlook.com>
+//
+// CppSheets is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// CppSheets is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with CppSheets. If not, see <https://www.gnu.org/licenses/>.
 #include <bargraph.hh>
 #include <QDialog>
 #include <QList>
