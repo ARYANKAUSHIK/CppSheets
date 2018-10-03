69331ebeb08f50719788bd946aa8ba27e61f4cce
patrickf2000 <patrick_dev2000@outlook.com>
2018-10-03 07:00:54 -0400

    update linux deploy script

--- deploy_linux.sh
+++ deploy_linux.sh
@@ -3,6 +3,7 @@ cd build
 mkdir linux_bin
 cp src/CppSheets linux_bin
 cp xlnt/source/libxlnt.{so,so.1.2} linux_bin
+cp libgraph/libgraph.so linux_bin
 cp ../linux/linux_install.sh linux_bin
 cp ../linux/linux_uninstall.sh linux_bin
 cp ../share/linux_deploy_readme linux_bin/README
