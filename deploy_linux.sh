5f1290dd798b15cd9a8889e6f766f42c9e074281
Patrick F <dev@patrickflynn.co>
2019-07-28 20:33:12 -0400

    updated deploy scripts

--- deploy_linux.sh
+++ deploy_linux.sh
@@ -4,6 +4,7 @@ mkdir linux_bin
 cp src/CppSheets linux_bin
 cp xlnt/source/libxlnt.{so,so.1.2} linux_bin
 cp libgraph/libgraph.so linux_bin
+cp libmath/libmath.so linux_bin
 cp ../linux/linux_install.sh linux_bin
 cp ../linux/linux_uninstall.sh linux_bin
 cp ../share/linux_deploy_readme linux_bin/README
--- linux/linux_install.sh
+++ linux/linux_install.sh
@@ -2,6 +2,8 @@
 echo "Installing..."
 cp CppSheets /usr/bin
 cp libxlnt.{so,so.1.2} /usr/lib
+cp libgraph.so /usr/local/lib
+cp libmath.so /usr/local/lib
 cp CppSheets.desktop /usr/share/applications
 cp CppSheets.svg /usr/share/icons/hicolor/48x48
 ldconfig
